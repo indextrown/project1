19286890a10308beaf55ae5fe36c271ac4a4a41c
indextrown <69367698+indextrown@users.noreply.github.com>
2021-08-29 23:07:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,64 @@
-from bs4 import BeautifulSoup # html분석
+
 import requests
-from apscheduler.schedulers.blocking import BlockingScheduler
+from bs4 import BeautifulSoup
 
-req = requests.get("http://ncov.mohw.go.kr/")
-# print(req.text)
+response = requests.get("http://paullab.synology.me/stock.html")
 
-soup = BeautifulSoup(req.text, "html.parser") # 파싱
+response.encoding = 'utf-8'
+html = response.text
 
-국내, 해외 = soup.find("div", class_="liveNum_today_new").find_all("span", class_="data")
-국내, 해외 = str(국내.text), str(해외.text)
-if "," in 국내:
-  국내 = 국내.replace(",", "")
-if "," in 해외:
-  해외 = 해외.replace(",", "")
-합계 = int(국내) + int(해외)
+soup = BeautifulSoup(html, 'html.parser')
+soup.select('#update')[0].text
+soup.select('.table > tbody > tr')[1]
+#@title
+oneStep = soup.select('.main')[2]
+twoStep = oneStep.select('tbody > tr')[1:]
+twoStep[0].select('td')[1].text.replace(',' , '')
 
-# print(합계)
+날짜 = []
+종가 = []
+전일비 = []
+거래량 = []
+for i in twoStep:
+    날짜.append(i.select('td')[0].text)
+    종가.append(int(i.select('td')[1].text.replace(',', '')))
+    전일비.append(int(i.select('td')[2].text.replace(',', '')))
+    거래량.append(int(i.select('td')[6].text.replace(',', '')))
 
 
-ads = soup.find("div", class_="t_dp_n m_dp_n").find_all("img")
-for i in ads:
-  print(i.get_text())
+%matplotlib inline
+import matplotlib.pyplot as plt
 
+plt.plot(날짜, 종가)
+plt.xticks(rotation = -45)
+plt.show
+l = []
 
-코로나 = "국내발생 : " + 국내 + "\n해외발생 : " + 해외 + "\n 합계 : " + str(합계)
-print(코로나)
+for i in range(len(날짜)):
+    l.append({
+        '날짜':날짜[i],
+        '종가':종가[i],
+        '전일비':전일비[i],
+        '거래량':거래량[i],
+        })
+l
+
+import csv
+import json
+
+#파일을 한 번 쓴다.
+with open('data.js', "w", encoding="UTF-8-sig") as f_write:
+    json.dump(l, f_write, ensure_ascii=False, indent=4)
+
+#파일을 다시 읽는다.
+data = ""
+with open('data.js', "r", encoding="UTF-8-sig") as f:
+    line = f.readline()
+    while line:
+        data += line
+        line = f.readline()
+
+#파일에 변수명을 추가하여 다시 쓴다.
+final_data = f"var data = {data};"
+with open('data.js', "w", encoding="UTF-8-sig") as f_write:
+    f_write.write(final_data)
